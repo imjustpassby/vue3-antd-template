18f092b5d7b21f402e4ffc8812503a51e05a3f39
imjustpassby <260809970@qq.com>
2020-12-17 17:38:24 +0800

    feat: examples with watch,computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,13 @@
     <img alt="Vue logo" src="../assets/img/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <br />
-    <p>{{ receiveMsg.msg }}</p>
+    <p>{{ receiveMsg }}</p>
+    <p>{{ extractMsg }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed, watch } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 import Bus from '@/utils/eventBus'
 export default defineComponent({
@@ -17,15 +18,25 @@ export default defineComponent({
     HelloWorld
   },
   setup() {
-    const receiveMsg = ref({
-      msg: 'this is default msg, im waiting an msg from HelloWorld component!'
-    })
+    const receiveMsg = ref(
+      'this is default msg, im waiting an msg from HelloWorld component!'
+    )
+
+    const extractMsg = computed(() => `computed example: ${receiveMsg.value}`)
+
+    watch(
+      () => receiveMsg.value,
+      (cur, prev) => {
+        console.log(cur)
+        console.log(prev)
+      }
+    )
 
     Bus.on('hello', receive => {
-      receiveMsg.value = receive
+      receiveMsg.value = receive.msg
     })
 
-    return { receiveMsg }
+    return { receiveMsg, extractMsg }
   }
 })
 </script>
