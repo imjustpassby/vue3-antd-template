a2e095dced590a4a7c3fbe1004e0ee9422b44bd1
imjustpassby <260809970@qq.com>
2021-01-13 09:49:28 +0800

    🐛 request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,7 +5,7 @@ const request = axios.create({
   //   process.env.NODE_ENV === 'production'
   //     ? 'https://ipassby.cloud'
   //     : 'http://localhost:9111'
-  // baseURL: 'https://ipassby.cloud',
+  baseURL: 'https://ipassby.cloud'
 })
 
 // object对象存放每次new CancelToken生成的方法
@@ -15,11 +15,13 @@ const source: {
 
 // 每次请求前都会把api放在此数组中，响应成功后清除此请求api
 let requestList: string[] = []
+// 不需要取消请求的白名单
+const whitelist: string[] = []
 
 request.interceptors.request.use(config => {
   // 取消上一次url相同的请求
   const url = config.url
-  if (requestList.length && url! in source) {
+  if (requestList.length && url! in source && !whitelist.includes(url!)) {
     console.log(`ready to cancel${url}`)
     cancelRequest(url!)
   }
@@ -59,27 +61,7 @@ request.interceptors.response.use(
     return Promise.resolve(res)
   },
   (err: AxiosError) => {
-    if (axios.isCancel(err)) {
-      // 根据业务场景确定是否需要清空
-      // 例如：页面跳转前，清空离开页面的请求
-      // requestList.length = 0
-      const fullUrl = err.config.url
-      const frontIndex = fullUrl!.indexOf('/api')
-      const tailIndex =
-        fullUrl!.indexOf('?') > 0 ? fullUrl!.indexOf('?') : fullUrl!.length
-      const url = fullUrl!.substring(frontIndex, tailIndex)
-      delete source[url!]
-      console.log(err.message)
-
-      return Promise.reject(err)
-    }
     return Promise.reject(err)
-
-    /* if (err.response!.status == 504) {
-      Vue.prototype.$message.warning('服务器错误...');
-    } else if (err.response!.status == 301) {
-      Vue.prototype.$message.warning('请登录之后再体验该功能喔...');
-    } */
   }
 )
 
@@ -95,37 +77,4 @@ export function cancelRequest(api: string, allCancel?: boolean) {
   }
 }
 
-function uuid(len: number) {
-  const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(
-    ''
-  )
-  let i
-  const uuid = []
-
-  if (len) {
-    // Compact form
-    for (i = 0; i < len; i++) {
-      uuid[i] = chars[0 | (Math.random() * chars.length)]
-    }
-  } else {
-    // rfc4122, version 4 form
-    let r
-
-    // rfc4122 requires these characters
-    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
-    uuid[14] = '4'
-
-    // Fill in random data.  At i==19 set the high bits of clock sequence as
-    // per rfc4122, sec. 4.1.5
-    for (i = 0; i < 36; i++) {
-      if (!uuid[i]) {
-        r = 0 | (Math.random() * 16)
-        uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r]
-      }
-    }
-  }
-
-  return uuid.join('')
-}
-
-export default request
+export default request.request
